e35f3fea70fb9b0893258351055b14908acbed00
Gleison De Paula <127432092+gleisondepaula@users.noreply.github.com>
2024-03-28 22:33:01 -0300

    Add files via upload

---
+++ prova_logicaProgamacao/script.js
@@ -0,0 +1,6 @@
+var nomeUsuario = prompt("Qual é o seu nome?");
+var dadoInteressante = prompt("Nos conte uma curiosidade sobre você:");
+
+document.write("<h3>O que " + nomeUsuario + " nos contou:</h3>");
+document.write("<p>" + dadoInteressante + "</p>");
+document.write("<p>Obrigado por compartilhar! Tenha um ótimo dia!</p>");
